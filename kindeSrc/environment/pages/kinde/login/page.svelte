c68bd61f36e3f054bb9d10166b7317a08c671d19
zeineb <zeineb.hamdi@hyfen.nl>
2025-08-13 09:38:43 +0200

    test with minimal version

---
+++ kindeSrc/environment/pages/kinde/login/page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../../globals.css";
+</script>
+
+<svelte:head>
+  <title>Sign In</title>
+</svelte:head>
+
+<div
+  class="dark flex min-h-screen w-full items-center justify-center bg-background p-4"
+>
+  <div
+    class="w-full max-w-md rounded-lg border border-border bg-card p-8 text-card-foreground shadow-lg"
+  >
+    {"{{kinde_login_form}}"}
+  </div>
+</div>
