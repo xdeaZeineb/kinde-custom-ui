a9d4f87248bd6b59548d738e570f2c623294696b
zeineb <zeineb.hamdi@hyfen.nl>
2025-08-13 13:25:40 +0200

    rename default folder

--- kindeSrc/environment/pages/(default)/page.js
+++ kindeSrc/environment/pages/(default)/page.js
@@ -2,6 +2,7 @@ export default function LoginPage() {
   return (
     <div className=" flex min-h-screen w-full items-center justify-center bg-background p-4">
       <div className="w-full max-w-md rounded-lg border border-border bg-card p-8 text-card-foreground shadow-lg">
+        <p>this is layout test</p>
         {/* This placeholder will be replaced by Kinde's server
           with the functional sign-in form.
         */}
